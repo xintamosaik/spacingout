371efb643063a9097e089f1e7cebf7c8a65a2c04
Ulf Dellbrügge <xintamosaik@gmail.com>
2025-08-09 14:31:21 +0200

    start building up space game

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,32 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
+#include <optional>
+class Ship {
+public:
+  int x;
+  int y;
+  Ship() {
+    x = 50;
+    y = 50;
+  }
+};
 
-int main()
-{
-    auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "Spacing Out");
-    window.setFramerateLimit(144);
+int main() {
+  auto window = sf::RenderWindow(sf::VideoMode({1920u, 1080u}), "Spacing Out");
+  window.setFramerateLimit(144);
 
-    while (window.isOpen())
-    {
-        while (const std::optional event = window.pollEvent())
-        {
-            if (event->is<sf::Event::Closed>())
-            {
-                window.close();
-            }
-        }
+  Ship one;
 
-        window.clear();
-        window.display();
+  std::cout << one.x;
+
+  while (window.isOpen()) {
+    while (const std::optional event = window.pollEvent()) {
+      if (event->is<sf::Event::Closed>()) {
+        window.close();
+      }
     }
+
+    window.clear();
+    window.display();
+  }
 }
